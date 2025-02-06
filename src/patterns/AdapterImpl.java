d7bf80522a733e0510959f4499aaaae6da4ed9b2
vaibgoya <vaibhav.g.goyal@oracle.com>
2025-02-06 19:21:09 +0530

    Adding Adapter design pattern

---
+++ src/patterns/AdapterImpl.java
@@ -0,0 +1,47 @@
+package patterns;
+
+interface InvoiceDetails {
+    public double getTotalAmountInUSD();
+    public double getTotalWeightInLBS();
+}
+
+class InvoiceDetailsImpl implements InvoiceDetails {
+    public double getTotalAmountInUSD() {
+        return 150.00;
+    }
+    public double getTotalWeightInLBS() {
+        return 200.00;
+    }
+}
+
+class InvoiceForIndia{
+    public int getTotalAmountInINR() {
+        return 10000;
+    }
+    public int getTotalWeightInKG() {
+        return 10;
+    }
+
+}
+
+class InvoiceAdaptor implements InvoiceDetails {
+
+    private InvoiceForIndia invoiceForIndia;
+    public InvoiceAdaptor(InvoiceForIndia invoiceForIndia) {
+        this.invoiceForIndia = invoiceForIndia;
+    }
+    public double getTotalAmountInUSD() {
+        return invoiceForIndia.getTotalAmountInINR()/87;
+    }
+    public double getTotalWeightInLBS() {
+        return invoiceForIndia.getTotalWeightInKG()*2.2;
+    }
+
+}
+
+public class AdapterImpl {
+    public static void main(String[] args) {
+        InvoiceAdaptor invoiceAdaptor = new InvoiceAdaptor(new InvoiceForIndia());
+        System.out.println(invoiceAdaptor.getTotalAmountInUSD());
+    }
+}
--- src/patterns/AdaptorImpl.java
+++
@@ -1,4 +0,0 @@
-package patterns;
-
-public class AdaptorImpl {
-}

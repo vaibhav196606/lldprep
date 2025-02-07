4f1fd76c3e78a79dfeb431f1c668cde0fbe3912c
vaibgoya <vaibhav.g.goyal@oracle.com>
2025-02-07 21:12:27 +0530

    Adding abstract factory design pattern

--- src/patterns/AbstractFactoryImpl.java
+++ src/patterns/AbstractFactoryImpl.java
@@ -1,4 +1,78 @@
 package patterns;
 
+interface Button{
+    void click();
+}
+
+interface Checkbox{
+    void click();
+}
+
+class DarkButton implements Button{
+    @Override
+    public void click() {
+        System.out.println("Dark button clicked");
+    }
+}
+
+class LightButton implements Button{
+    @Override
+    public void click() {
+        System.out.println("Light button clicked");
+    }
+}
+
+class DarkCheckbox implements Checkbox{
+    @Override
+    public void click() {
+        System.out.println("Dark checkbox clicked");
+    }
+}
+
+class LightCheckbox implements Checkbox{
+    @Override
+    public void click() {
+        System.out.println("Light checkbox clicked");
+    }
+}
+
+interface UI{
+    public Button getButton();
+    public Checkbox getCheckbox();
+}
+
+class DarkUI implements UI{
+    public Button getButton() {
+        return new DarkButton();
+    }
+    public Checkbox getCheckbox() {
+        return new DarkCheckbox();
+    }
+}
+
+class LightUI implements UI{
+    public Button getButton() {
+        return new LightButton();
+    }
+    public Checkbox getCheckbox() {
+        return new DarkCheckbox();
+    }
+}
+
+class Application{
+    public Button button;
+    public Checkbox checkbox;
+    public UI ui;
+    Application(UI ui){
+        this.ui = ui;
+        button = ui.getButton();
+        checkbox = ui.getCheckbox();
+    }
+}
+
 public class AbstractFactoryImpl {
+    public static void main(String[] args) {
+        Application app = new Application(new LightUI());
+        app.button.click();
+    }
 }
